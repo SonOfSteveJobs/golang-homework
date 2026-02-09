3cb8d08ac11e2ada5fc064df4499df1a42b60477
Evgeniy Artemov <evgenev2222@gmail.com>
2026-02-09 20:39:36 +0300

    feat: getTableRecords

--- db_explorer/db_explorer.go
+++ db_explorer/db_explorer.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 // тут вы пишете код
@@ -22,32 +25,165 @@ type Column struct {
 	HasDefaultValue sql.NullString
 }
 
+type ResMap map[string]interface{}
+
 func (d *DBInfo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	method := r.Method
-	fmt.Println("PATH:", path, "METHOD:", method)
+	// params := r.URL.Query().Get("limit")
 
-	if method == http.MethodGet && path == "/" {
-		tableNames := make([]string, 0, len(d.Tables))
-		for tableName := range d.Tables {
-			tableNames = append(tableNames, tableName)
+	tableNames := d.GetTables()
+
+	if path == "/" {
+		if method != http.MethodGet {
+			resMap := ResMap{
+				"error": "unknown method (only GET supported)",
+			}
+			createJSONResponse(w, http.StatusBadRequest, resMap)
+			return
 		}
 
-		resMap := map[string]interface{}{
-			"response": map[string]interface{}{
+		resMap := ResMap{
+			"response": ResMap{
 				"tables": tableNames,
 			},
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(resMap)
+		createJSONResponse(w, http.StatusOK, resMap)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(d.Tables)
+	pathSegments := strings.Split(path, "/")
+	var table string
+	var id int
+
+	switch len(pathSegments) {
+	case 2:
+		table = pathSegments[1]
+	case 3:
+		table = pathSegments[1]
+		idStr := pathSegments[2]
+		if idStr != "" {
+			idInt, err := strconv.Atoi(idStr)
+			if err != nil || idInt <= 0 {
+				resMap := ResMap{
+					"error": "invalid id, must be positive integer",
+				}
+				createJSONResponse(w, http.StatusBadRequest, resMap)
+				return
+			}
+
+			id = idInt
+		}
+	default:
+		resMap := ResMap{
+			"error": "invalid path",
+		}
+		createJSONResponse(w, http.StatusBadRequest, resMap)
+		return
+	}
+
+	if !slices.Contains(tableNames, table) {
+		resMap := ResMap{
+			"error": "unknown table",
+		}
+		createJSONResponse(w, http.StatusNotFound, resMap)
+		return
+	}
+
+	resMap := ResMap{}
+
+	if table != "" {
+		records, err := d.getTableRecords(table, id)
+		if err != nil {
+			fmt.Printf("Error getting table records: %v\n", err)
+			resMap["error"] = err.Error()
+			createJSONResponse(w, http.StatusInternalServerError, resMap)
+			return
+		}
+
+		resMap["response"] = map[string][]ResMap{"records": records}
+	}
+	createJSONResponse(w, http.StatusOK, resMap)
+}
+
+func (d *DBInfo) GetTables() []string {
+	tableNames := make([]string, 0, len(d.Tables))
+	for name := range d.Tables {
+		tableNames = append(tableNames, name)
+	}
+	slices.Sort(tableNames)
+	return tableNames
+}
+
+func (d *DBInfo) getTableRecords(tableName string, id int) ([]ResMap, error) {
+	colTypeByName := make(map[string]string, len(d.Tables[tableName]))
+	for _, c := range d.Tables[tableName] {
+		colTypeByName[c.Name] = strings.ToLower(c.Type)
+	}
+
+	var (
+		rows *sql.Rows
+		err  error
+	)
+
+	if id > 0 {
+		query := fmt.Sprintf("SELECT * FROM `%s` WHERE id = ?", tableName)
+		rows, err = d.DB.Query(query, id)
+	} else {
+		query := fmt.Sprintf("SELECT * FROM `%s`", tableName)
+		rows, err = d.DB.Query(query)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	var records []ResMap
+
+	for rows.Next() {
+		values := make([]interface{}, len(columns))
+		valuePtrs := make([]interface{}, len(columns))
+		for i := range values {
+			valuePtrs[i] = &values[i]
+		}
+
+		err := rows.Scan(valuePtrs...)
+		if err != nil {
+			return nil, err
+		}
+
+		record := make(ResMap, len(columns))
+		for i, colName := range columns {
+			val := values[i]
+			record[colName] = val
+
+			b, ok := val.([]byte)
+			if ok {
+				colType := colTypeByName[colName]
+
+				if strings.HasPrefix(colType, "int") {
+					intVal, err := strconv.Atoi(string(b))
+					if nil == err {
+						record[colName] = intVal
+						continue
+					}
+				}
+				record[colName] = string(b)
+			}
+		}
+		records = append(records, record)
+	}
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return records, nil
 }
 
 func NewDbExplorer(db *sql.DB) (http.Handler, error) {
@@ -130,4 +266,10 @@ ORDER BY ORDINAL_POSITION;
 	}
 
 	return columnsSlice, nil
+}
+
+func createJSONResponse(w http.ResponseWriter, status int, data ResMap) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
 }
